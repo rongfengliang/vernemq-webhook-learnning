d388cc97f45de4009b769feb857bd622b41fd5b2
rong <1141591465@qq.com>
2020-11-28 21:13:31 +0800

    add haproxy

---
+++ client/app.js
@@ -0,0 +1,15 @@
+var mqtt = require('mqtt')
+var client  = mqtt.connect('mqtt://127.0.0.1')
+ 
+client.on('connect', function () {
+  client.subscribe('demoapp', function (err) {
+    if (!err) {
+      client.publish('presence', 'Hello mqtt')
+    }
+  })
+})
+ 
+client.on('message', function (topic, message) {
+  // message is Buffer
+  console.log(message.toString())
+})
